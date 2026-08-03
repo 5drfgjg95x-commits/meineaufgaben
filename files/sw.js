1a2dce6788cf1510f09412754e689a56984c045c
5drfgjg95x-commits <5drfgjg95x@privaterelay.appleid.com>
2026-08-04 01:54:11 +0200

    Update sw.js

--- files/sw.js
+++ files/sw.js
@@ -1,52 +1,40 @@
-const CACHE_NAME = "aufgaben-app-v2";
+const CACHE_NAME = "aufgaben-v2";
 
 const FILES_TO_CACHE = [
-  "./",
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.json",
-
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png"
+  "./manifest.json"
 ];
 
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-
     caches.open(CACHE_NAME)
-      .then(cache => {
-
-        return cache.addAll(FILES_TO_CACHE);
-
-      })
-
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
 
+  self.skipWaiting();
 });
 
 
 self.addEventListener("activate", event => {
 
   event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
 
-    caches.keys()
-      .then(keys => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
 
-        return Promise.all(
-
-          keys
-            .filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-
-        );
-
-      })
-
+        })
+      )
+    )
   );
+
+  self.clients.claim();
 
 });
 
@@ -56,9 +44,9 @@ self.addEventListener("fetch", event => {
   event.respondWith(
 
     caches.match(event.request)
-      .then(response => {
+      .then(cached => {
 
-        return response || fetch(event.request);
+        return cached || fetch(event.request);
 
       })
 
